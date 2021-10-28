f9459038f39d2119264d2fffdf11c69c72083852
mrochadh <mrochadh@digitalhouse.com>
2021-10-28 14:56:25 -0300

    criando itens da lista

--- 4_manipulando_nos/js/script.js
+++ 4_manipulando_nos/js/script.js
@@ -1,50 +1,76 @@
 // a árvore dos elementos/nós do documento HTML e o DOM
 
-// pegar um elemento
-let a = document.querySelector('a');
+console.log(document.querySelector('h2').innerText);
+
+console.log(document.querySelector('div h2').innerText);
+
+
+// pegar um elemento/nó (node)
+let a = document.querySelector('a'); // pega o 1º elemento <a>
 
 // alterar conteúdo interno e estilos
-a.innerText += ' com confiança!';
+a.innerText += ' com muita confiança!';
 a.style.fontSize = '30px';
 
 
-// funções para manipular atributos de um nó: has, get, set, 
-console.log(a.hasAttribute('src')); //false
-console.log(a.hasAttribute('href')); //true
-console.log(a.getAttribute('href')); // o valor do atributo
+// funções para manipular atributos de um nó: has, get, set, remove
+console.log(a.hasAttribute('href'));
+
+/*
+if (a.hasAttribute('href')) {
+  console.log('tem ref');
+} else {
+  console.log('sem ref');
+}
+*/
+
+a.hasAttribute('href') ? console.log(a.getAttribute('href')) : console.log('sem ref');
+
 a.setAttribute('href', 'http://www.digitalhouse.com/br');
+
 a.removeAttribute('href');
 
-// criar um nó parágrafo
-let p = document.createElement('p');
-// inserir conteúdo
-//p.innerText = "lorem ipsum picsum mussum ipsum";
-let txt = document.createTextNode('lorem ipsummm criado com text node');
-p.appendChild(txt);
-p.style.color = 'red';
 
+// criar um nó parágrafo
+let par = document.createElement('p');
+// inserir conteúdo e style
+par.innerText = "meu novo parágrafo pelo js";
+par.style.color = 'red';
 // adicionar ao final da página
-//document.querySelector('body').appendChild(p);
-document.body.appendChild(p);
+document.body.appendChild(par);
 
 // remover o parágrafo
-//document.body.removeChild(p);
+document.body.removeChild(par);
 
+// adicionando outro parágrafo dentro da primeira div
+let par2 = document.createElement('p');
+par2.innerText = "um outro parágrafo";
+par2.style.backgroundColor = 'yellow';
+document.querySelector('div').appendChild(par2);
 
 // criar uma lista com ordenação do tipo "a" contendo 3 itens
 let lista = document.createElement('ol');
-lista.setAttribute('type', 'a');
+lista.style.listStyleType = 'lower-latin';
+//lista.setAttribute('type', 'a');
 
+/*
 let item = document.createElement('li');
-item.innerText = 'primeiro item da lista';
-
+item.innerText = 'primeiro item';
 lista.appendChild(item);
+*/
+
+for (let i = 1; i <= 5; i++) {
+  let item = document.createElement('li');
+  item.innerText = `${i}o elemento da lista`;
+  lista.appendChild(item);
+}
 
 // adicionar ao final da página
 document.body.appendChild(lista);
 
 
-// remover o primeiro item da lista
-
+// remover o segundo item da lista
+let itens = document.querySelectorAll('li');
+lista.removeChild(itens[1]);
 
 
